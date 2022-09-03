db1deaef60dabdad0214fbfb0dd12fb51f8ddfef
Charlie <2295509496@qq.com>
2022-09-03 22:17:13 +0800

    私有属性和方法代码

---
+++ 08_面向对象基础/ch_17_私有属性和方法.py
@@ -0,0 +1,19 @@
+class Woman:
+
+    def __init__(self, name) -> None:
+        
+        self.name = name
+        self.__age = 18
+
+    def __secret(self):
+        # 在对象的方法内部，是可以访问对象的私有属性的
+        print("%s 的年龄是 %d" % (self.name, self.__age))
+
+
+a = Woman("小芳")
+
+# 私有属性，在外界不能直接访问
+# print(a.__age)
+
+# 私有方法，同样不允许在外界直接访问
+# a.__secret()
